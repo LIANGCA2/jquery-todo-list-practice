45a136d92b20c0506dd832f7fc2d1e7380a7ac9b
LIANGCA2 <CAROL T T LIANG (ITA-ISDC-ISD-OOCLL/ZHA) carol.t.t.liang@oocl.com>
2018-07-17 21:54:45 +0800

    finish optimize the function of add and tab

--- js/todo_new.js
+++ js/todo_new.js
@@ -1,67 +1,122 @@
 $(document).ready(function () {
     const todoForm = {
-        todos:{
-          id:"",
-          name:"",
-          complet:"",
-        },
+        todos:[],
         statusOfList:"all"
     }
 
 
     window.addItem = (event)=>{
-        var toAdd = $(event.target).val();
-        console.log(toAdd);
-        todoForm.todos.push({id:generateUUID(),name:toAdd,complete:false});
+       var input = $(event.target).siblings()[0];
+        var toAdd = $(input).val();
 
+        todoForm.todos.push({id:generateUUID(),name:toAdd,complete:false});
+render()
+    }
+    window.checkComplete = (currentCompleteStatus)=>{
+        if(currentCompleteStatus == "false"){
+            return "checked";
+        }else{
+            return "";
+        }
+    }
+    window.checkItem = (viewId) =>{
+        //var viewId = view.id;
+        let checkedItem = todoForm.todos.find((item)=>item.id === viewId) ;
+        if(checkedItem!==undefined){
+            checkedItem.complete = !checkedItem.complete;
+        }
+        render();
+    }
+    window.filterByStatus = (todo,status)=>{
+
+        const  filterBys = (x,status)=>{
+            if(status==="all")
+                return true;
+            else if(status === "complete")
+                return x.complete;
+            else
+                return !x.complete;
+        }
+
+        var list = todo.filter((x)=> filterBys(x,status)).map((x)=>{
+
+        todoLi = ()=>{return `<li id="${x.id}" class="${checkComplete(x.complete)}" onclick = "checkItem(${x.id})">
+                <input name="done-todo" type="checkbox" class="done-todo">${x.name}</li>`;}
+                return todoLi();
+        });
+        console.log(list);
+        return list;
     }
 
 
 
-})
 
 
-let todoList = `<div>
+
+window.showStatus = (currentStatus,status)=>{
+        if(currentStatus == status){
+            return "selected";
+        }else{
+            return "";
+        }
+    }
+    window.showTodoList = (status) =>{
+        todoForm.statusOfList = status;
+        render();
+    }
+     todoList = ()=>{
+        return `<div>
             <input class="input-text" type="text" name="ListItem" data-com.agilebits.onepassword.user-edited="yes">
             <div id="button" onclick = "addItem(event)">Add</div>
         </div>
         <br>
         <ol>
+        ${filterByStatus(todoForm.todos,todoForm.statusOfList).join("")}
         </ol>
-
         <div>
             <ul id="filters">
                 <li>
-                    <a href="#" data-filter="all" >ALL</a>
+                    <a href="#" data-filter="all" class="${showStatus(todoForm.statusOfList,'all')}" onclick = "showTodoList('all')" >ALL</a>
                 </li>
                 <li>
-                    <a href="#" data-filter="active" class="">Active</a>
+                    <a href="#" data-filter="active" class="${showStatus(todoForm.statusOfList,'active')}" onclick = "showTodoList('active')">Active</a>
                 </li>
                 <li>
-                    <a href="#" data-filter="complete" class="">Complete</a>
+                    <a href="#" data-filter="complete" class="${showStatus(todoForm.statusOfList,'complete')}" onclick = "showTodoList('complete')">Complete</a>
                 </li>
             </ul>
 
-        </div>`
+        </div>`}
 
 
 
-function generateUUID() {
-    /*jshint bitwise:false */
-    var i,
-        random;
-    var uuid = '';
+    function generateUUID() {
+        /*jshint bitwise:false */
+        var i,
+            random;
+        var uuid = '';
 
-    for (i = 0; i < 32; i++) {
-        random = Math.random() * 16 | 0;
-        if (i === 8 || i === 12 || i === 16 || i === 20) {
-            uuid += '-';
+        for (i = 0; i < 32; i++) {
+            random = Math.random() * 16 | 0;
+            if (i === 8 || i === 12 || i === 16 || i === 20) {
+                uuid += '-';
+            }
+            uuid += (i === 12
+                ? 4
+                : (i === 16
+                    ? (random & 3 | 8)
+                    : random)).toString(16);
         }
-        uuid += (i === 12
-            ? 4
-            : (i === 16
-                ? (random & 3 | 8)
-                : random)).toString(16);
+        return uuid;
     }
-    return uuid;
+const render = ()=>{
+    // console.log(todoList);
+        $("#todo").html(todoList())
+    console.log(`${filterByStatus(todoForm.todos,todoForm.statusOfList).join("")}`);
+
+
 }
+render();
+})
+
+
