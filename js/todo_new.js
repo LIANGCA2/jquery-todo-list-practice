883bad3f4a6d5cfb947333dc953162d55f6346bd
LIANGCA2 <CAROL T T LIANG (ITA-ISDC-ISD-OOCLL/ZHA) carol.t.t.liang@oocl.com>
2018-07-18 09:10:55 +0800

    finish check and uncheck

--- js/todo_new.js
+++ js/todo_new.js
@@ -1,93 +1,97 @@
 $(document).ready(function () {
     const todoForm = {
-        todos:[],
-        statusOfList:"all"
+        todos: [],
+        statusOfList: "all"
     }
 
 
-    window.addItem = (event)=>{
-       var input = $(event.target).siblings()[0];
+    window.addItem = (event) => {
+        var input = $(event.target).siblings()[0];
         var toAdd = $(input).val();
 
-        todoForm.todos.push({id:generateUUID(),name:toAdd,complete:false});
-render()
+        todoForm.todos.push({id: generateUUID(), name: toAdd, complete: false});
+        render()
     }
-    window.checkComplete = (currentCompleteStatus)=>{
-        if(currentCompleteStatus == "false"){
-            return "checked";
-        }else{
+    window.checkComplete = (currentCompleteStatus) => {
+        if (currentCompleteStatus == false) {
             return "";
+        } else {
+            return "checked";
         }
     }
-    window.checkItem = (viewId) =>{
-        //var viewId = view.id;
-        let checkedItem = todoForm.todos.find((item)=>item.id === viewId) ;
-        if(checkedItem!==undefined){
+    window.checkItem = (event) => {
+        var viewId = $(event.target).parent("li").attr("id");
+
+        let checkedItem = todoForm.todos.find((item) => item.id === viewId);
+        console.log(viewId);
+        if (checkedItem !== undefined) {
             checkedItem.complete = !checkedItem.complete;
         }
         render();
     }
-    window.filterByStatus = (todo,status)=>{
 
-        const  filterBys = (x,status)=>{
-            if(status==="all")
+
+
+
+    window.filterByStatus = (todo, status) => {
+
+        const filterBys = (x, status) => {
+            if (status === "all")
                 return true;
-            else if(status === "complete")
+            else if (status === "complete")
                 return x.complete;
             else
                 return !x.complete;
         }
 
-        var list = todo.filter((x)=> filterBys(x,status)).map((x)=>{
+        var list = todo.filter((x) => filterBys(x, status)).map((x) => {
 
-        todoLi = ()=>{return `<li id="${x.id}" class="${checkComplete(x.complete)}" onclick = "checkItem(${x.id})">
-                <input name="done-todo" type="checkbox" class="done-todo">${x.name}</li>`;}
-                return todoLi();
+            todoLi = () => {
+                return `<li id="${x.id}" class="${checkComplete(x.complete)}" >
+                <input name="done-todo" type="checkbox" class="done-todo" ${x.complete ? 'checked' : ""} onclick = "checkItem(event)" >${x.name}</li>`;
+            }
+            return todoLi();
         });
-        console.log(list);
+
         return list;
     }
 
 
-
-
-
-
-window.showStatus = (currentStatus,status)=>{
-        if(currentStatus == status){
+    window.showStatus = (currentStatus, status) => {
+        if (currentStatus == status) {
             return "selected";
-        }else{
+        } else {
             return "";
         }
     }
-    window.showTodoList = (status) =>{
+    window.showTodoList = (status) => {
         todoForm.statusOfList = status;
         render();
     }
-     todoList = ()=>{
+    todoList = () => {
         return `<div>
             <input class="input-text" type="text" name="ListItem" data-com.agilebits.onepassword.user-edited="yes">
             <div id="button" onclick = "addItem(event)">Add</div>
         </div>
         <br>
         <ol>
-        ${filterByStatus(todoForm.todos,todoForm.statusOfList).join("")}
+        ${filterByStatus(todoForm.todos, todoForm.statusOfList).join("")}
         </ol>
         <div>
             <ul id="filters">
                 <li>
-                    <a href="#" data-filter="all" class="${showStatus(todoForm.statusOfList,'all')}" onclick = "showTodoList('all')" >ALL</a>
+                    <a href="#" data-filter="all" class="${showStatus(todoForm.statusOfList, 'all')}" onclick = "showTodoList('all')" >ALL</a>
                 </li>
                 <li>
-                    <a href="#" data-filter="active" class="${showStatus(todoForm.statusOfList,'active')}" onclick = "showTodoList('active')">Active</a>
+                    <a href="#" data-filter="active" class="${showStatus(todoForm.statusOfList, 'active')}" onclick = "showTodoList('active')">Active</a>
                 </li>
                 <li>
-                    <a href="#" data-filter="complete" class="${showStatus(todoForm.statusOfList,'complete')}" onclick = "showTodoList('complete')">Complete</a>
+                    <a href="#" data-filter="complete" class="${showStatus(todoForm.statusOfList, 'complete')}" onclick = "showTodoList('complete')">Complete</a>
                 </li>
             </ul>
 
-        </div>`}
-
+        </div>`
+    }
 
 
     function generateUUID() {
@@ -109,14 +113,15 @@ window.showStatus = (currentStatus,status)=>{
         }
         return uuid;
     }
-const render = ()=>{
-    // console.log(todoList);
+
+    const render = () => {
+
         $("#todo").html(todoList())
-    console.log(`${filterByStatus(todoForm.todos,todoForm.statusOfList).join("")}`);
 
 
-}
-render();
+
+    }
+    render();
 })
 
 
